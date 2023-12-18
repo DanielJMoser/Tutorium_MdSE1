664245de5a06b35934242af6ca3c59aa7642b47c
Daniel <danieljmoser@protonmail.com>
2023-12-18 12:19:21 +0100

    Adding comments

--- Uez3/pointerarithmetik.c
+++ Uez3/pointerarithmetik.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char *argv[]) {
+    // Überprüfen, ob mindestens ein Kommandozeilenargument (neben dem Programmnamen) vorhanden ist
     if (argc < 2) {
-        printf("Fehler! Keine Zeichenkette übergeben.\n");
-        return 1;
+        // Wenn kein Argument übergeben wurde, Fehlermeldung ausgeben und Programm beenden
+        fprintf(stderr, "Fehler: Keine Zeichenkette als Argument übergeben.\n");
+        return 1; // Beendet das Programm mit Exit-Code 1 (Fehlerzustand)
     }
 
+    // Zuweisung des ersten Kommandozeilenarguments (nach dem Programmnamen) zur Variable str
     char *str = argv[1];
 
+    // Initialisierung eines Arrays zur Speicherung der Häufigkeiten jedes ASCII-Zeichens
+    // Das Array hat 256 Elemente (entspricht der Anzahl der möglichen ASCII-Zeichen)
+    // Alle Elemente werden zu Beginn auf 0 gesetzt
     int frequency[256] = {0};
 
+    // Durchlaufen der Zeichenkette str mit einem Zeiger ptr
+    // Die Schleife läuft, bis das Nullzeichen '\0' (das Ende der Zeichenkette) erreicht wird
     for (char *ptr = str; *ptr != '\0'; ptr++) {
-        frequency[(unsigned char) *ptr]++;
+        // Erhöht den Zähler für das entsprechende ASCII-Zeichen im frequency-Array
+        // Der ASCII-Wert des Zeichens wird als Index verwendet
+        frequency[(unsigned char)*ptr]++;
     }
 
+    // Durchlaufen aller möglichen ASCII-Zeichen
     for (int i = 0; i < 256; i++) {
-
+        // Überprüfen, ob das aktuelle Zeichen in der Zeichenkette vorkommt (Häufigkeit > 0)
         if (frequency[i] > 0) {
+            // Ausgabe des Zeichens und seiner Häufigkeit
+            // %c gibt das Zeichen aus, %d die zugehörige Häufigkeit
             printf("Zeichen: '%c' - Häufigkeit: %d\n", i, frequency[i]);
         }
     }
-    return 0;
+
+    return 0; // Beendet das Programm erfolgreich
 }
--- Uez3/upper.c
+++ Uez3/upper.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
+// TODO:
+//      Eine Zeichenkette mit einer maximalen Länge von 49 Zeichen einlesen (um Buffer-Overflow zu vermeiden).
+//      Jeden Kleinbuchstaben in der Zeichenkette in den entsprechenden Großbuchstaben umwandeln.
+//      Die veränderte Zeichenkette ausgeben.
+
 int main() {
-    char str[50];
+    char str[50]; // Erstellt einen String mit Platz für 49 Zeichen + Null-Terminator
 
     printf("Bitte geben Sie eine Zeichenfolge ein: ");
-    scanf("%49[^\n]", str);
+    scanf("%49[^\n]", str); // Liest bis zu 49 Zeichen ein
 
+    // Durchläuft die Zeichenkette und wandelt Kleinbuchstaben in Großbuchstaben um
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
             str[i] = str[i] - 'a' + 'A';
         }
     }
-    printf("%s\n", str);
+
+    printf("Veränderte Zeichenfolge: %s\n", str); // Gibt die veränderte Zeichenkette aus
+
     return 0;
 }
